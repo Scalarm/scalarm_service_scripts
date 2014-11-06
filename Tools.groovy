9160efa1a1987420f4658d1cd6b0eadb4b29e1bb
Jakub Liput <jakub.liput@gmail.com>
2014-11-06 16:12:45 +0100

    Update Tools.groovy
    Fixing GPG RVM install bug

--- Tools.groovy
+++ Tools.groovy
@@ -73,6 +73,7 @@ public class Tools
 
     def installRvmRuby() {
         println 'installing RVM...'
+        optionalCommand('gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3')
         def installCmd = "\\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1"
         command(installCmd)['out']
     }
