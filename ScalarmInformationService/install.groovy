cc01adc74e078b9a34768e0d35dfd7e7fd15cf92
Dariusz Król <dkrol@agh.edu.pl>
2016-03-30 15:26:58 +0200

    Paasage: another env variable changed

--- ScalarmInformationService/install.groovy
+++ ScalarmInformationService/install.groovy
@@ -1,7 +1,5 @@
 def tools = new Tools(this.args)
 
-// ---
-
 tools.installCurl()
 
 if (!tools.isRubyValid()) tools.installRvmRuby()
@@ -12,12 +10,11 @@ tools.command("wget ${tools.config.downloadPath} -O ${tools.installDir}/archive.
 
 new AntBuilder().sequential {
     unzip(src: "${tools.installDir}/archive.zip", dest: tools.installDir, overwrite: true)
-    move(file: "${tools.installDir}/${tools.config.serviceName}-${tools.config.scalarmTag}", tofile: tools.serviceDir)  
-    
+    move(file: "${tools.installDir}/${tools.config.serviceName}-${tools.config.scalarmTag}", tofile: tools.serviceDir)
+
     configFiles.each() { copy(file: it, todir: tools.serviceConfigDir) }
 }
 
 tools.command('bundle install', tools.serviceDir)
 tools.command('rake db:migrate', tools.serviceDir, ['RAILS_ENV': 'production'])
 tools.command('rake db:migrate', tools.serviceDir)
-
