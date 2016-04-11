1bdfac0727b13632d7f0cba2389e8352f98c55f8
groundnuty <groundnuty@gmail.com>
2016-04-11 11:36:53 +0200

    added dependencies for molecular dynamics simulation to be installed alongside SM dependencies

--- ScalarmSimulationManager/install.groovy
+++ ScalarmSimulationManager/install.groovy
@@ -1,6 +1,12 @@
 def tools = new Tools(this.args)
 
-tools.command("sudo apt-get -y install zip")
+tools.command("sudo apt-get -y install ruby zip bc mencoder wget libboost-dev libboost-thread-dev libtiff5-dev libopenexr-dev")
+
+tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray_3.7.0.0-4~ubuntu14.04.1_amd64.deb -O /tmp/povray37.deb")
+tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray-includes_3.7.0.0-4~ubuntu14.04.1_all.deb  -O /tmp/povray37_include.deb")
+
+tools.command("dpkg -i /tmp/povray37.deb")
+tools.command("dpkg -i /tmp/povray37_include.deb")
 
 // to execute sample simulation, which has executor in ruby
 if (!tools.isRubyValid()) tools.installRvmRuby()
