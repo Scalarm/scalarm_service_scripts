f92fa83bde1ba691552f89afd15194d1bc982f48
Dariusz Król <dkrol@agh.edu.pl>
2016-08-02 13:12:55 +0200

    Adding curl dependency to simulation manager installation.

--- ScalarmSimulationManager/install.groovy
+++ ScalarmSimulationManager/install.groovy
@@ -1,6 +1,6 @@
 def tools = new Tools(this.args)
 
-tools.command("sudo apt-get -y install ruby zip bc mencoder wget libboost-dev libboost-thread-dev libtiff5-dev libopenexr-dev")
+tools.command("sudo apt-get -y install ruby zip bc mencoder wget libboost-dev libboost-thread-dev libtiff5-dev libopenexr-dev curl wget")
 
 tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray_3.7.0.0-4~ubuntu14.04.1_amd64.deb -O /tmp/povray37.deb")
 tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray-includes_3.7.0.0-4~ubuntu14.04.1_all.deb  -O /tmp/povray37_include.deb")
@@ -15,4 +15,3 @@ tools.download(tools.config.simDownloadURL, "${tools.installDir}/scalarm_simulat
 tools.command("unxz scalarm_simulation_manager.xz && chmod +x scalarm_simulation_manager", tools.installDir)
 
 ant = new AntBuilder()
-
