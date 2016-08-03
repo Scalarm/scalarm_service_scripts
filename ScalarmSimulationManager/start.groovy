96319c34abf661dddf9ea86a416de0756efd4277
Dariusz Krol <dkrol@agh.edu.pl>
2016-08-03 14:25:50 +0200

    Change the way SiM is started.

--- ScalarmSimulationManager/start.groovy
+++ ScalarmSimulationManager/start.groovy
@@ -16,7 +16,7 @@ new File("${tools.installDir}/config.json").withWriter { out ->
 }
 
 // execution moved to .sh file
-tools.command("bash --login -c \"./scalarm_simulation_manager\"", tools.installDir)
+tools.command("bash --login -c \"./scalarm_simulation_manager\"", tools.serviceDir)
 // pushd $HOME/.cloudify/scalarm_simulation_manager
     // bash --login -c "./scalarm_simulation_manager"
 // popd
