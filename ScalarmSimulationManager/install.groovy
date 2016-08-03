834305b8bc12f253d54ab556811c5e62207898a5
Dariusz Krol <dkrol@agh.edu.pl>
2016-08-03 13:42:21 +0200

    Changing links to povray packages.

--- ScalarmSimulationManager/install.groovy
+++ ScalarmSimulationManager/install.groovy
@@ -2,14 +2,16 @@ def tools = new Tools(this.args)
 
 tools.command("sudo apt-get -y install ruby zip bc mencoder wget libboost-dev libboost-thread-dev libtiff5-dev libopenexr-dev curl wget")
 
-tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray_3.7.0.0-4~ubuntu14.04.1_amd64.deb -O /tmp/povray37.deb")
-tools.command("wget http://ftp.sa.xemacs.org/pub/ubuntu-releases/ubuntu/pool/universe/p/povray/povray-includes_3.7.0.0-4~ubuntu14.04.1_all.deb  -O /tmp/povray37_include.deb")
+tools.command("wget http://ftp.ubuntu.com/ubuntu/ubuntu/pool/universe/p/povray/povray_3.7.0.0-4~ubuntu14.04.1_amd64.deb -O /tmp/povray37.deb")
+tools.command("wget http://ftp.ubuntu.com/ubuntu/ubuntu/pool/universe/p/povray/povray-includes_3.7.0.0-4~ubuntu14.04.1_all.deb -O /tmp/povray37_include.deb")
 
 tools.command("dpkg -i /tmp/povray37.deb")
 tools.command("dpkg -i /tmp/povray37_include.deb")
 
 // to execute sample simulation, which has executor in ruby
 if (!tools.isRubyValid()) tools.installRvmRuby()
+
+ant.mkdir(dir: tools.installDir)
 
 tools.download(tools.config.simDownloadURL, "${tools.installDir}/scalarm_simulation_manager.xz")
 tools.command("unxz scalarm_simulation_manager.xz && chmod +x scalarm_simulation_manager", tools.installDir)
